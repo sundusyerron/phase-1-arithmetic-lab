da960e761333ef69f21f78ac5219bbf0af4085f8
Sundus <sundusaden105@gmail.com>
2022-11-24 16:29:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+//function multiplication
+let num1 = 1;
+let num2 = 62;
+const multiply = num2 * num1;
+ let number = 5;
+const random = 1;
+
+// function mod
+let num3 = 29;
+let num4 = 5;
+const mod= num3%num4;
+
+//function max 
+const array = [3,20,15,];
+const max = Math.max(...array);
